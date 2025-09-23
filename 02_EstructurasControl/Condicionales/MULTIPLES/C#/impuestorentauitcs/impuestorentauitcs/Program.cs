853b1285b70a21a1be79046015717e74ceed159f
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-23 15:35:13 -0500

    agregando ejercicio multiples cs

---
+++ 02_EstructurasControl/Condicionales/MULTIPLES/C#/impuestorentauitcs/impuestorentauitcs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impuestorentauitcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el monto de su renta: ");
+            double renta = double.Parse(Console.ReadLine());
+
+            double UIT = 5250; 
+            double limite = 7 * UIT;
+            double impuesto;
+
+            if (renta > limite)
+            {
+                impuesto = renta * 0.05;
+                Console.WriteLine("Debe pagar impuesto de: " + impuesto);
+            }
+            else
+            {
+                impuesto = 0;
+                Console.WriteLine("No paga impuesto.");
+            }
+        }
+    }
+    }
+}
---
+++ 02_EstructurasControl/Condicionales/MULTIPLES/C#/impuestorentauitcs/impuestorentauitcs/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("impuestorentauitcs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("impuestorentauitcs")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("c54f2840-a113-4874-bad7-660b6f991a55")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
