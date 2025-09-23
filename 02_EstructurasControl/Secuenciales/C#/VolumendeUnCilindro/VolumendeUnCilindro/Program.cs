7f1a7dcdc9125eb46670e9f021dfc67bd573571a
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-23 12:57:38 -0500

    agregando cambios en los archivos para mejorar el ordenamiento de los mismos02_EstructurasControl/Secuenciales/C#

---
+++ 02_EstructurasControl/Secuenciales/C#/VolumendeUnCilindro/VolumendeUnCilindro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumendeUnCilindro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float h;
+            float r;
+            double pi= 3.1415929;
+            double abc;
+            double v;
+            Console.WriteLine("Hallar volumen de un Cilindro");
+            Console.WriteLine("Ingre halura del cilindro");
+            h = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar radio del cilindro");
+            r = float.Parse(Console.ReadLine());
+
+            abc = pi * r * r;
+            v = abc * h;
+
+            Console.WriteLine($"el cilindro de : \nradio: {r}\nhaltura: {h}\narea de su base: {abc}\nvolumen: {v}");
+
+
+
+
+        }
+    }
+}
