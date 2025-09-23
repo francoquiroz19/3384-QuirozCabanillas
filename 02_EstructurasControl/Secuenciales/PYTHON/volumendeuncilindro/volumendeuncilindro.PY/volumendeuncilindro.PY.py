ba7865c79c611cbc961fd20c5226e296a5de3349
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-23 12:59:02 -0500

    agregando otro commit para el ordenamiento de los archivos

---
+++ 02_EstructurasControl/Secuenciales/PYTHON/volumendeuncilindro/volumendeuncilindro.PY/volumendeuncilindro.PY.py
@@ -0,0 +1,12 @@
+
+pi=3.1415929
+
+h = float(input("Ingrese altura del cilindro: "))
+r = float(input("Ingrese radio del cilindro: "))
+
+
+abc = pi * r * r
+v = abc * h
+
+print(f"El cilindro de:\nradio: {r}\naltura: {h}\narea de su base: {abc}\nvolumen: {v}")
+
