6ff95f96c7d31ec68baeb81b86176470818943c0
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-23 11:55:04 -0500

    agregando caarpeta con ejercicio de 'areayperimetro de un terreno recta' transcrito a python

---
+++ 02_EstructurasControl/Secuenciales/areayperimetroterrnrectangular.PY/areayperimetroterrnrectangular/areayperimetroterrnrectangular.py
@@ -0,0 +1,12 @@
+# Calculando área y perímetro de un terreno rectangular
+print("Calculando area y Perimetro de un Terreno Rectangular")
+
+l1 = float(input("Ingrese primer lado del terreno: "))
+l2 = float(input("Ingrese segundo lado del terreno: "))
+
+area = l1 * l2
+perimetro = 2*l1 + 2*l2
+
+print(f"area: {area}")
+print(f"Perimetro: {perimetro}")
+
