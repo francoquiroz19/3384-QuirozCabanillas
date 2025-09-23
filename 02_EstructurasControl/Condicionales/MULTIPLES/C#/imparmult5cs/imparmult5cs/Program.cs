f8b0e2c000a50a0f0a9af196369ad2b263a810d4
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-23 15:38:34 -0500

    agregando ejercicio 2 multiples cs

---
+++ 02_EstructurasControl/Condicionales/MULTIPLES/C#/imparmult5cs/imparmult5cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imparmult5cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 != 0 && num % 5 == 0)
+            {
+                Console.WriteLine(num + " es impar y múltiplo de 5.");
+            }
+            else
+            {
+                if (num % 2 == 0 && num % 5 == 0)
+                    Console.WriteLine(num + " es par y múltiplo de 5.");
+                else if (num % 2 != 0 && num % 5 != 0)
+                    Console.WriteLine(num + " es impar pero no múltiplo de 5.");
+                else
+                    Console.WriteLine(num + " es par y no múltiplo de 5.");
+            }
+        }
+    }
+}
