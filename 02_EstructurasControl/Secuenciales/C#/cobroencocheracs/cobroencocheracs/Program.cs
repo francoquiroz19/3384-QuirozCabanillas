6d2509695528586e0f323d4e730cb52c3e299f42
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-24 23:17:27 -0500

    agregando ejercicio cs  de E.C secuenciales cobrodeestacionamiento

---
+++ 02_EstructurasControl/Secuenciales/C#/cobroencocheracs/cobroencocheracs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cobroencocheracs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hallar el costo total a pagar por estacionamiento si el monto por
+hora cuesta S/. 2.5*/
+            Console.WriteLine("COSTO DE ESTACIONAMIENTO s/.2,5/h");
+            Console.WriteLine("Ingrese las oras del uso de estacionamiento: ");
+            float h = float.Parse(Console.ReadLine());
+            var ce = 0.25 * h;
+            Console.WriteLine($"Su costo de estacionamiento es de {ce}");
+            Console.ReadLine();
+        }
+    }
+}
