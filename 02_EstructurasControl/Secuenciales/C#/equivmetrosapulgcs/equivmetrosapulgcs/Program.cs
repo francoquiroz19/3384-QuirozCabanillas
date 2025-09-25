fa8ffa33c03a614e351e7bd36fc1e7325fcb43b2
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-24 22:49:33 -0500

    agregando ejercicio cs  de E.C secuenciales equivmetrosapulgcs

---
+++ 02_EstructurasControl/Secuenciales/C#/equivmetrosapulgcs/equivmetrosapulgcs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equivmetrosapulgcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Calcular el equivalente de metros en pulgadas*/
+            Console.WriteLine("Calcular el equivalente de metros en pulgadas");
+            Console.WriteLine("Ingrese la cantidad de metros: ");
+            double met = double.Parse(Console.ReadLine());
+
+            double pulgadas = met * 39.3701;
+
+            
+            Console.WriteLine(met + " metros <> " + pulgadas + " pulgadas.");
+
+        }
+    }
+}
