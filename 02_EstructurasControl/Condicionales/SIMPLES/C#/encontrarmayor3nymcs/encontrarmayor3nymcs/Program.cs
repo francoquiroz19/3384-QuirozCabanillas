b6d753a589cd350ba56463f7b3aeea4e6fc07971
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-24 22:20:39 -0500

    agregando ejercicio condicionales cs encontrando mayor de 3 num

---
+++ 02_EstructurasControl/Condicionales/SIMPLES/C#/encontrarmayor3nymcs/encontrarmayor3nymcs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encontrarmayor3nymcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            int mayor = a;
+
+            if (b > mayor)
+                mayor = b;
+            if (c > mayor)
+                mayor = c;
+
+            Console.WriteLine("El mayor es: " + mayor);
+        }
+    }
+}
