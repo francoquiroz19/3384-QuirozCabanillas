60cdf2773b0a7c22d8c5635c8880d627dd9fc158
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-24 20:48:17 -0500

    agregando segundo archivo cs repetitivas ejercico 2 vernumimparesyposics

---
+++ 02_EstructurasControl/repetitivas/FOR/C#/vernumimparesyposics/vernumimparesyposics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vernumimparesyposics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Un programa que lea n números enteros y muestre cuántos son impares y positivos*/
+
+            int n, numero, contador = 0;
+
+            Console.WriteLine("Ingrese la cantidad de números: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+                {
+                Console.WriteLine($"Ingrese número {i} :");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > 0 && numero % 2 != 0)
+                {
+                    contador++;
+                }
+
+            }
+            Console.WriteLine($"La cantidad de números positivos enteros es {contador}");
+        }
+    }
+}
