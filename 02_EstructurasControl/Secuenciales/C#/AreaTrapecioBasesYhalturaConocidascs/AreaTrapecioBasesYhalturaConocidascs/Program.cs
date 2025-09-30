befe32604054c866b0b59476938748b00eeaa211
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-29 22:25:21 -0500

    Agregando ejercicio AreaTrapecioBasesYhalturaConocidascs de secuenciales en cs

---
+++ 02_EstructurasControl/Secuenciales/C#/AreaTrapecioBasesYhalturaConocidascs/AreaTrapecioBasesYhalturaConocidascs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTrapecioBasesYhalturaConocidascs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallando area de un trapecio de:\nBase mayor: 18 \nBase menor: 10\n" +
+                "Haltura: 8");
+            var (bma, bme, h) = (18, 10, 8);
+            double at = ((bma + bme) / 2) * h;
+            Console.WriteLine($"Area del trapecio: {at}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("formula usada: ((base mayor + base menor)/2)*haltura");
+            Console.ReadLine();
+
+
+        }
+    }
+}
