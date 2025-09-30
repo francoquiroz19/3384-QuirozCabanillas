4fc320a8c6cc8aa5872a5bc67fdd0faebb2047de
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-09-29 21:52:16 -0500

    Agregando ejercicio longitudDeUnaCircunferenciacs de secuenciales en cs

---
+++ 02_EstructurasControl/Secuenciales/C#/longitudDeUnaCircunferenciacs/longitudDeUnaCircunferenciacs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longitudDeUnaCircunferenciacs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hallar longitud de una circunferencia");
+
+            Console.WriteLine("Ingresar medida del radio(metros)");
+            double radio = double.Parse(Console.ReadLine());
+
+            double lgc = radio * Math.PI * 2;
+            Console.WriteLine($"Longitud de la circunferenia: {lgc} metros");
+           
+        }
+    }
+}
