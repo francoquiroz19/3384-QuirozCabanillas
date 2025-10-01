9bf4d44d90487b734549dd765d18883f11d0b0c5
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-10-01 18:54:15 -0500

    agregando ejercicio de condicional multiples usando SWITCH para ver estado civil en base a un caracter

---
+++ 02_EstructurasControl/Condicionales/MULTIPLES/C#/LeerCaractDarEstdCivilcs/LeerCaractDarEstdCivilcs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerCaractDarEstdCivilcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ec;
+            Console.WriteLine("Ingrese el estado civil (s, c, d, v): ");
+            ec = char.Parse(Console.ReadLine());
+
+            switch (ec)
+            { 
+            case 'S':
+                Console.WriteLine("Soltero");
+                break;
+
+            case 'C':
+                Console.WriteLine("Casado");
+                break;
+            case 'D':
+                Console.WriteLine("Divorciado");
+                break;
+            case 'V':
+                Console.WriteLine("Viudo");
+                break;
+            default:
+                Console.WriteLine("Estado civil no valido");
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
