a2586aa86493578671b6440b64d986d2261c75ad
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-10-04 17:16:14 -0500

    agregando ejercicio de repetitivas /CantidadDigitosDeUnNumeroFORcs

---
+++ 02_EstructurasControl/repetitivas/FOR/C#/CantidadDigitosDeUnNumeroFORcs/CantidadDigitosDeUnNúmeroFORcs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantidadDigitosDeUnNúmeroFORcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, digitos=0;
+            Console.WriteLine("determinar # digitos de un número");
+            Console.WriteLine("Ingrese un número entero porfavor");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero<0)
+            {
+                numero = -numero;
+            }
+
+            if (numero == 0)
+            {
+                digitos = 1;
+            }
+            else
+            {
+                for (; numero > 0; numero = numero / 10)
+                {
+                    digitos++;
+                }
+            }
+
+            Console.WriteLine("El número tiene " + digitos + " dígitos");
+        }
+    }
+}
+s
