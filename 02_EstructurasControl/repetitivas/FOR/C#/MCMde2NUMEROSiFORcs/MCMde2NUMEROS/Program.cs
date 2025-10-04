06338f4b908b60d20d7273245241d758848bb99e
FrancoYarenQuirozCabaillas <N00500542@upn.pe>
2025-10-03 19:10:41 -0500

    agregando ejercicio de repetitivas(FOR) /MCMde2NUMEROSiFORcs

---
+++ 02_EstructurasControl/repetitivas/FOR/C#/MCMde2NUMEROSiFORcs/MCMde2NUMEROS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCMde2NUMEROS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Encuentre el mcm de dos números a y b sabiendo que (a)(b) = (mcm)(mcd).*/
+            Console.Write("Ingrese el primer número (a): ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número (b): ");
+            int b = int.Parse(Console.ReadLine());
+
+            int mcd = 1;
+
+           
+            int menor;
+            if (a < b)
+                menor = a;
+            else
+                menor = b;
+
+            
+            for (int i = 1; i <= menor; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    mcd = i;
+                }
+            }
+
+           
+            int mcm = (a * b) / mcd;
+
+            Console.WriteLine($"El MCD de {a} y {b} es: {mcd}");
+            Console.WriteLine($"El MCM de {a} y {b} es: {mcm}");
+        }
+    }
+}
